e291d93572259f2c2920324b9e6719c00ecad543
gzmarisa <gzmarisa@email.arizona.edu>
2024-03-08 16:07:32 +0200

    Outlined python code

---
+++ Code/Code/Calibrate.py
@@ -0,0 +1,42 @@
+## Upload relevant libraries
+
+## Upload PA data
+
+
+## Upload SAAQIS data
+
+## Fix the 
+
+## 
+
+## Remove outliers from PA data
+## Remove values from PM2.5 data
+## Remove negative values
+## Check for  high values 
+## Remove high values
+## Remove double values 
+
+## Remove values from RH data
+## Remove negative values
+## Check for  high values 
+## Remove high values
+## Remove double values 
+
+## Remove values from temperature data
+## Remove negative values
+## Check for  high values 
+## Remove high values
+## Remove double values 
+
+
+## Remove outliers from SAAQIS data
+## Remove values from PM2.5 data
+## Remove negative values
+## Check for  high values 
+## Remove high values
+## Remove double values 
+
+
+
+
+
