111b0f2f8fa3a38d5a2973bca727ab7341fa79ea
gzmarisa <gzmarisa@email.arizona.edu>
2024-03-11 08:42:48 +0200

    Started to create code for uploading PA data

--- Code/Calibrate.py
+++ Code/Calibrate.py
@@ -2,11 +2,6 @@
 ## Upload relevant libraries
 import csv
 
-## Upload PA data
-
-## Upload SAAQIS data
-#csv_file = open("Welgegund.csv")
-
 ## Define functions
 def empty(pm):
     return len(pm) == 0
@@ -14,7 +9,27 @@ def empty(pm):
 def negative(pm):
     return float(pm) < 0
 
+## Upload PA data
+with open('../Data/PurpleAir/CSIR_wel_eb7e/CSIR_wel_eb7e.csv', newline='') as PA_csv_file:
+    PA_csv_reader = csv.reader(PA_csv_file, delimiter=',')
+    
+    PA_started = False
+    PA_counter = 0
+    PA_SumPms = 0
+    PA_valid = 0
+    for row in PA_csv_reader:
+        if PA_counter < 10:
+            date = row[0].split(",")[0]
+            date = date.split(" ")
+            date = date.split(",")
+            print(date)
+        
+        PA_counter += 1
 
+
+
+
+## Upload SAAQIS data
 
 with open('../Data/SAAQIS/Welgegund-NAQI/Welgegund.csv', newline='') as csvfile:
     csv_reader = csv.reader(csvfile, delimiter=',')
@@ -97,6 +112,71 @@ elif int(hour) < 24 and pms != None
 ## Check for  high values 
 ## Remove high values
 ## Remove double values 
+
+
+
+"""
+Orginal code I don't want to lose
+## Upload relevant libraries
+import csv
+
+## Upload PA data
+
+## Upload SAAQIS data
+#csv_file = open("Welgegund.csv")
+
+## Define functions
+def empty(pm):
+    return len(pm) == 0
+
+def negative(pm):
+    return float(pm) < 0
+
+
+
+with open('../Data/SAAQIS/Welgegund-NAQI/Welgegund.csv', newline='') as csvfile:
+    csv_reader = csv.reader(csvfile, delimiter=',')
+    
+    started = False
+    counter = 0
+    sumPms = 0
+    valid = 0
+    for row in csv_reader: #row is the row of csv file
+        if counter > 6 and counter < 26285:       
+            #hour = row[0].split(" ")[0]
+            date = row[0].split(" ")[1]
+            if date == "01/10/2018" or started: #start date
+                started = True
+                date = date.split("/") 
+                hour = row[0].split(" ")[0].split(":")[0]
+                pms = row[1]
+                #print(date, pms)
+                if empty(pms) or negative(pms): # add function for weird values
+                    pms = None
+                if int(hour) == 24 and pms != None:
+                    sumPms += float(pms)
+                    avgPms = sumPms/valid
+                    valid = 0
+                    print(date, avgPms)
+                    
+                    sumPms = 0 #resart after the 24 hours
+                elif int(hour) == 24 and valid == 0:
+                    print(date, 0)
+                    SumPms = 0
+                elif int(hour) == 24 and pms == None:
+                    avgPms = sumPms/valid
+                    valid = 0
+                    print(date, avgPms)
+                    SumPms = 0
+              
+                elif int(hour) < 24 and pms != None:
+                    sumPms += float(pms)
+                    valid += 1
+
+
+                
+               
+        counter += 1
 
 
 
