fd55baebfde7aa96307b0b83952a6227a05714ae
gzmarisa <gzmarisa@email.arizona.edu>
2024-04-24 08:40:32 +0200

    Updated Upload_AirQo.py for device g5126 and started for g5113

--- Code/Upload_AirQo.py
+++ Code/Upload_AirQo.py
@@ -104,33 +104,33 @@ path_AirQo = '../Data/AirQo/Raw/'
 #     writer.writerows(combined_data_AirQo_g5113)
 
 
-list_airqo_g5126_PM25_raw = []
-list_airqo_g5126_PM10_raw = []
-list_airqo_g5126_timestamp_raw = []
-#list_airqo_g5113_timestamp_sast_raw = []
+# list_airqo_g5126_PM25_raw = []
+# list_airqo_g5126_PM10_raw = []
+# list_airqo_g5126_timestamp_raw = []
+# #list_airqo_g5113_timestamp_sast_raw = []
 
-# Go through CSV file with 
-with open('20240423_airqo_aqg5126_raw_combined.csv', newline='') as csv_file:
-    reader = csv.reader(csv_file, delimiter=',')
-    for i,row in enumerate(reader):
-        if i > 0:
-            #print(row)
-            airqo_g5126_timestamp_raw = row[9]
-            airqo_g5126_timestamp_raw = datetime.strptime(airqo_g5126_timestamp_raw, '%Y-%m-%d %H:%M:%S')
-            list_airqo_g5126_timestamp_raw.append(airqo_g5126_timestamp_raw)
-            #print(airqo_g5113_timestamp_raw)
+# # Go through CSV file with 
+# with open('20240423_airqo_aqg5126_raw_combined.csv', newline='') as csv_file:
+#     reader = csv.reader(csv_file, delimiter=',')
+#     for i,row in enumerate(reader):
+#         if i > 0:
+#             #print(row)
+#             airqo_g5126_timestamp_raw = row[9]
+#             airqo_g5126_timestamp_raw = datetime.strptime(airqo_g5126_timestamp_raw, '%Y-%m-%d %H:%M:%S')
+#             list_airqo_g5126_timestamp_raw.append(airqo_g5126_timestamp_raw)
+#             #print(airqo_g5113_timestamp_raw)
             
-            airqo_g5126_PM25_raw = row[7]
-            if not_available(airqo_g5126_PM25_raw):
-                airqo_g5126_PM25_raw = np.nan
-            list_airqo_g5126_PM25_raw.append(float(airqo_g5126_PM25_raw))
-            #print(airqo_g5126_PM25_raw)
+#             airqo_g5126_PM25_raw = row[7]
+#             if not_available(airqo_g5126_PM25_raw):
+#                 airqo_g5126_PM25_raw = np.nan
+#             list_airqo_g5126_PM25_raw.append(float(airqo_g5126_PM25_raw))
+#             #print(airqo_g5126_PM25_raw)
             
-            airqo_g5126_PM10_raw = row[5]
-            if not_available(airqo_g5126_PM10_raw):
-                airqo_g5126_PM10_raw = np.nan
-            list_airqo_g5126_PM10_raw.append(float(airqo_g5126_PM10_raw))
-            #print(airqo_g5126_PM10_raw)        
+#             airqo_g5126_PM10_raw = row[5]
+#             if not_available(airqo_g5126_PM10_raw):
+#                 airqo_g5126_PM10_raw = np.nan
+#             list_airqo_g5126_PM10_raw.append(float(airqo_g5126_PM10_raw))
+#             #print(airqo_g5126_PM10_raw)        
 
 
 list_airqo_g5113_PM25_raw = []
@@ -139,42 +139,43 @@ list_airqo_g5113_timestamp_raw = []
 #list_airqo_g5113_timestamp_sast_raw = []
             
 # Go through CSV file with 
-# with open('20240423_airqo_aqg5113_raw_combined.csv', newline='') as csv_file:
-#     reader = csv.reader(csv_file, delimiter=',')
-#     for i,row in enumerate(reader):
-#         if i > 0 and i < 10:
-#             #print(row)
-#             airqo_g5113_timestamp_raw = row[9]
-#             airqo_g5113_timestamp_raw = datetime.strptime(airqo_g5113_timestamp_raw, '%Y-%m-%d %H:%M:%S')
-#             list_airqo_g5113_timestamp_raw.append(airqo_g5113_timestamp_raw)
-#             #print(airqo_g5113_timestamp_raw)
+with open('20240423_airqo_aqg5113_raw_combined.csv', newline='') as csv_file:
+    reader = csv.reader(csv_file, delimiter=',')
+    for i,row in enumerate(reader):
+        if i > 0 and i < 10:
+            #print(row)
+            airqo_g5113_timestamp_raw = row[9]
+            #airqo_g5113_timestamp_raw = datetime.strptime(airqo_g5113_timestamp_raw, '%Y-%m-%d %H:%M:%S')
+            #list_airqo_g5113_timestamp_raw.append(airqo_g5113_timestamp_raw)
+            print(airqo_g5113_timestamp_raw)
             
-#             airqo_g5113_PM25_raw = row[7]
-#             if not_available(airqo_g5113_PM25_raw):
-#                 airqo_g5113_PM25_raw = np.nan
-#             list_airqo_g5113_PM25_raw.append(float(airqo_g5113_PM25_raw))
-#             #print(airqo_g5113_PM25_raw)
+            # airqo_g5113_PM25_raw = row[7]
+            # if not_available(airqo_g5113_PM25_raw):
+            #     airqo_g5113_PM25_raw = np.nan
+            # list_airqo_g5113_PM25_raw.append(float(airqo_g5113_PM25_raw))
+            # #print(airqo_g5113_PM25_raw)
             
-#             airqo_g5113_PM10_raw = row[5]
-#             if not_available(airqo_g5113_PM10_raw):
-#                 airqo_g5113_PM10_raw = np.nan
-#             list_airqo_g5113_PM10_raw.append(float(airqo_g5113_PM10_raw))
-#             #print(airqo_g5113_PM10_raw)                    
+            # airqo_g5113_PM10_raw = row[5]
+            # if not_available(airqo_g5113_PM10_raw):
+            #     airqo_g5113_PM10_raw = np.nan
+            # list_airqo_g5113_PM10_raw.append(float(airqo_g5113_PM10_raw))
+            # #print(airqo_g5113_PM10_raw)                    
 
+## Plots for AirQo G5126
 ## PM25 Raw Data for AirQo G5126
-plt.scatter(list_airqo_g5126_timestamp_raw, list_airqo_g5126_PM25_raw, alpha=0.5, marker="x")
-#plt.ylim(0, 100)
-plt.xticks(rotation=45, ha='right')
-plt.xlabel("Date")
-plt.ylabel("PM$_{2.5}$ ($\mu$g/m$^3$)")
-plt.title("PM$_{2.5}$ Raw Data")
-#plt.locator_params(axis='x', nbins=12)
-plt.grid()
+# plt.scatter(list_airqo_g5126_timestamp_raw, list_airqo_g5126_PM25_raw, alpha=0.5, marker="x")
+# #plt.ylim(0, 100)
+# plt.xticks(rotation=45, ha='right')
+# plt.xlabel("Date")
+# plt.ylabel("PM$_{2.5}$ ($\mu$g/m$^3$)")
+# plt.title("PM$_{2.5}$ Raw Data")
+# #plt.locator_params(axis='x', nbins=12)
+# plt.grid()
 
-## PM10 Plots 
-## PM10 RAW Data
-# plt.scatter(list_timestamp_sast_raw, list_PM10_raw, alpha=0.5, marker="x")
-# plt.ylim(0, 1000)
+# ## PM10 Plots 
+# ## PM10 RAW Data
+# plt.scatter(list_airqo_g5126_timestamp_raw, list_airqo_g5126_PM10_raw, alpha=0.5, marker="x")
+# #plt.ylim(0, 1000)
 # plt.xticks(rotation=45, ha='right')
 # plt.xlabel("Date")
 # plt.ylabel("PM$_{10}$ ($\mu$g/m$^3$)")
@@ -182,8 +183,9 @@ plt.grid()
 # #plt.locator_params(axis='x', nbins=12)
 # plt.grid()
 
-## PM25 Raw Data
-# plt.scatter(list_timestamp_sast_raw, list_PM1_raw, alpha=0.5, marker="x")
+## Plots for AirQo G5113
+## PM25 Raw Data for AirQo G5113
+# plt.scatter(list_airqo_g5113_timestamp_raw, list_airqo_g5113_PM25_raw, alpha=0.5, marker="x")
 # #plt.ylim(0, 100)
 # plt.xticks(rotation=45, ha='right')
 # plt.xlabel("Date")
@@ -193,12 +195,12 @@ plt.grid()
 # plt.grid()
 
 ## PM10 Plots 
-## PM10 RAW Data
-# plt.scatter(list_timestamp_sast_raw, list_PM10_raw, alpha=0.5, marker="x")
-# plt.ylim(0, 1000)
-# plt.xticks(rotation=45, ha='right')
-# plt.xlabel("Date")
-# plt.ylabel("PM$_{10}$ ($\mu$g/m$^3$)")
-# plt.title("PM$_{10}$ Raw Data")
-# #plt.locator_params(axis='x', nbins=12)
-# plt.grid()
+## PM10 RAW Data for AirQo G5113
+plt.scatter(list_airqo_g5113_timestamp_raw, list_airqo_g5113_PM10_raw, alpha=0.5, marker="x")
+#plt.ylim(0, 1000)
+plt.xticks(rotation=45, ha='right')
+plt.xlabel("Date")
+plt.ylabel("PM$_{10}$ ($\mu$g/m$^3$)")
+plt.title("PM$_{10}$ Raw Data")
+#plt.locator_params(axis='x', nbins=12)
+plt.grid()
